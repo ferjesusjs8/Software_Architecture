a6dd2f46c6569f3dd450a0566204f7cea95ac030
fernando.js8 <fernando.js8@outlook.com>
2022-01-10 23:03:34 -0300

    04 - Polimorfismo Done

---
+++ SoftwareArchitecture/OOP/02 - Pilares OOP/04 - Polimorfismo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    public class CafeteiraExpressa : Eletrodomestico
+    {
+        public CafeteiraExpressa(string nome, int voltagem) : base(nome, voltagem)
+        {
+
+        }
+
+        public CafeteiraExpressa() : base("Padrao", 220)
+        {
+
+        }
+
+        private static void AquecerAgua() { }
+
+        private static void MoerGraos() { }
+
+        public void PrepararCafe()
+        {
+            AquecerAgua();
+            MoerGraos();
+        }
+
+        public override void Ligar()
+        {
+
+        }
+
+        public override void Desligar()
+        {
+
+        }
+    }
+}
