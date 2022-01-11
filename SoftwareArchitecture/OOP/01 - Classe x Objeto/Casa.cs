d9e22599b1c96d55349a9516dc51acaa568a1235
fernando.js8 <fernando.js8@outlook.com>
2022-01-10 22:33:06 -0300

    started - added folders - added Aula 1 and Aula 2

---
+++ SoftwareArchitecture/OOP/01 - Classe x Objeto/Casa.cs
@@ -0,0 +1,29 @@
+﻿namespace OOP._01___Classe_x_Objeto
+{
+    // Definicao Classe
+    public class Casa
+    {
+        public int Id { get; set; }
+        public decimal Metros2 { get; set; }
+        public int Andares { get; set; }
+        public decimal Valor { get; set; }
+        public int NumeroVagas { get; set; }
+    }
+
+    //Definicao Objeto
+    public class Objeto
+    {
+        public Objeto()
+        {
+            var casa = new Casa()
+            {
+                Andares = 3,
+                Id = 1,
+                Metros2 = 200000,
+                NumeroVagas = 15,
+                Valor = 2000000
+            };
+
+        }
+    }
+}
---
+++ SoftwareArchitecture/OOP/02 - Pilares OOP/EstadoComportamento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP
+{
+    public class Pessoa
+    {
+        // Estado / Informacoes / Propriedades
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        // Comportamento / Processa uma informacao e devolve outra, ou altera o estado da entidade
+        public int CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+            var idade = dataAtual.Year - DataNascimento.Year;
+
+            if (dataAtual < DataNascimento.AddYears(idade))
+            {
+                idade--;
+            }
+
+            return idade;
+        }
+    }
+}
