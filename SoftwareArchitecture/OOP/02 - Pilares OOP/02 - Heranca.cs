949ffb2c3d7aae4a3392a8c1fa7e6b44f139c899
fernando.js8 <fernando.js8@outlook.com>
2022-01-10 22:45:29 -0300

    Heranca Done

---
+++ SoftwareArchitecture/OOP/02 - Pilares OOP/02 - Heranca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP
+{
+    public class Funcionario : Pessoa
+    {
+        public DateTime DataAdmissao { get; set; }
+        public string Registro { get; set; }
+    }
+
+    public class Processo
+    {
+        public void Execucao()
+        {
+            var funcionario = new Funcionario()
+            {
+                DataAdmissao = DateTime.Now.AddYears(-2),
+                DataNascimento = DateTime.Now.AddYears(-30),
+                Nome = "Joao da Silva",
+                Registro = "ABCD1234"
+            };
+
+            funcionario.CalcularIdade();
+        }
+    }
+}
